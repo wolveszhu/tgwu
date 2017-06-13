3045e1f512506d4690279dd2286a149aade6ace2
wolveszhu <13203783812@163.com>
2017-06-13 11:29:38 +0800

    second

--- Application/Admin/Controller/LoginController.class.php
+++ Application/Admin/Controller/LoginController.class.php
@@ -8,6 +8,7 @@
 
 namespace Admin\Controller;
 use Think\Controller;
+use Common\Model;
 
 class LoginController extends Controller{
     public function index(){
@@ -15,6 +16,28 @@ class LoginController extends Controller{
     }
 
     public function check(){
+        $username = $_POST['username'];
+        $pwd = $_POST['password'];
 
+        if(!trim($username)){
+            return show(0,'用户名不能为空');
+        }
+
+        if(!trim($pwd)){
+            return show(0,'密码不能为空');
+        }
+
+        $res = D('user') -> getDetailByNickName($username);
+
+        if(!$res || $res['status'] != 1){
+            return show(0,'用户不存在');
+        }
+
+        if($res['password'] != getMd5Password($pwd)){
+            return show(0,'密码不正确');
+        }
+
+        session('adminUser',$res);
+        return show(1,'登录成功!');
     }
 }
