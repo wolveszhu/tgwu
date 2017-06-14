b6ffe439ca94cf2f3a744c7078afcf13178e1e0c
wolveszhu <13203783812@163.com>
2017-06-14 11:14:59 +0800

    second

---
+++ Application/Admin/Model/WaresModel.class.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator:stimmer
+ * Date: 2017/6/14
+ * Time: 11:02
+ */
+
+namespace Admin\Model;
+use Think\Model;
+
+class WaresModel extends Model{
+
+}
