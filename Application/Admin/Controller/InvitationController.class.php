c78dfef84a8facfd486eab5f88b9e1560e570bca
wolveszhu <13203783812@163.com>
2017-06-14 11:23:09 +0800

    second

---
+++ Application/Admin/Controller/InvitationController.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator:stimmer
+ * Date: 2017/6/14
+ * Time: 10:49
+ */
+
+namespace Admin\Controller;
+
+class InvitationController extends CommonController {
+    public function index(){
+
+    }
+}
