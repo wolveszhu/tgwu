f5883417cb0d184aa91dccd05ec36346612fb3a7
wolveszhu <13203783812@163.com>
2017-06-14 11:15:14 +0800

    second

---
+++ Application/Admin/Model/InvitationModel.class.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator:stimmer
+ * Date: 2017/6/14
+ * Time: 10:51
+ */
+
+namespace Admin\Model;
+use Think\Model;
+
+class InvitationModel extends Model {
+
+}
