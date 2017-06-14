7e63d49b1cec8ab5a0179dbc76775436e2bb9c10
wolveszhu <13203783812@163.com>
2017-06-14 11:23:23 +0800

    second

---
+++ Application/Admin/Controller/CarouselController.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator:stimmer
+ * Date: 2017/6/14
+ * Time: 10:45
+ */
+
+namespace Admin\Controller;
+
+class CarouselController extends CommonController {
+    public function index(){
+        $this -> display();
+    }
+}
