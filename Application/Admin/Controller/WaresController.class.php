db24a9603d8bb5904434ae4a313d11bdb88e6d83
wolveszhu <13203783812@163.com>
2017-06-14 11:22:49 +0800

    second

---
+++ Application/Admin/Controller/WaresController.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator:stimmer
+ * Date: 2017/6/14
+ * Time: 11:01
+ */
+
+namespace Admin\Controller;
+
+class WaresController extends CommonController {
+    public function index(){
+
+    }
+}
