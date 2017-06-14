707430819142b58e78c29996006003d62787f164
wolveszhu <13203783812@163.com>
2017-06-14 14:28:48 +0800

    second

--- Application/Common/Common/function.php
+++ Application/Common/Common/function.php
@@ -15,6 +15,18 @@ function  show($status, $message,$data=array()) {
 
     exit(json_encode($reuslt));
 }
+
 function getMd5Password($password) {
     return md5($password . C('MD5_PRE'));
+}
+
+function status($status) {
+    if($status == 0) {
+        $str = '关闭';
+    }elseif($status == 1) {
+        $str = '正常';
+    }elseif($status == -1) {
+        $str = '删除';
+    }
+    return $str;
 }
