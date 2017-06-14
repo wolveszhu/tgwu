829a8bc3bf4aaf0f12f1feb95dbbfbec9e352850
wolveszhu <13203783812@163.com>
2017-06-14 11:23:16 +0800

    second

---
+++ Application/Admin/Controller/CategoryController.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator:stimmer
+ * Date: 2017/6/14
+ * Time: 10:54
+ */
+
+namespace Admin\Controller;
+
+class CategoryController extends CommonController {
+    public function index(){
+
+    }
+}
