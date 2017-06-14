684bab8aeae1c419453f477ebf35e8b535e50a9d
wolveszhu <13203783812@163.com>
2017-06-14 17:29:48 +0800

    second

--- Application/Admin/Model/CarouselModel.class.php
+++ Application/Admin/Model/CarouselModel.class.php
@@ -15,4 +15,24 @@ class CarouselModel extends Model{
     public function __construct(){
         $this -> _db = M('carousel');
     }
+
+    public function getCarousels(){
+        $data = [
+            'carouselStatus' => 'desc',
+            'carouselId' => 'desc'
+        ];
+        return $this -> _db -> order($data) -> select();
+    }
+
+    public function getCarouselById($id){
+        return $this -> _db -> where('carouselId='.$id) -> find();
+    }
+
+    public function insert($data = array()){
+        if(!data || !is_array($data)){
+            return 0;
+        }
+
+        return $this -> _db ->add($data);
+    }
 }
