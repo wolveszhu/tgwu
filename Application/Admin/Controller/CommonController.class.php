dd5057696f96542fa1acc2fa731bc83feb598dce
wolveszhu <13203783812@163.com>
2017-06-14 09:58:31 +0800

    second

---
+++ Application/Admin/Controller/CommonController.class.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator:stimmer
+ * Date: 2017/6/13
+ * Time: 16:32
+ */
+
+namespace Admin\Controller;
+use Think\Controller;
+
+class CommonController extends Controller{
+    public function __construct(){
+        parent::__construct();
+        $this -> _init();
+    }
+
+    private function _init(){
+        $isLogin = $this -> isLogin();
+
+        if(!$isLogin){
+            $this -> redirect('admin.php?c=Login&a=index');
+        }
+    }
+
+    public function getLoginUser(){
+        return session('adminUser');
+    }
+
+    public function isLogin(){
+        $user = $this -> getLoginUser();
+
+        if($user && is_array($user)){
+            return true;
+        }
+
+        return false;
+    }
+}
