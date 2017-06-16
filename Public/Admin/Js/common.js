ffe526713646497c08fcd667819c267e9d11f068
wolveszhu <13203783812@163.com>
2017-06-16 16:55:14 +0800

    second

--- Public/Admin/Js/common.js
+++ Public/Admin/Js/common.js
@@ -93,7 +93,7 @@ $('#button-listorder').click(function() {
     $(data).each(function(i){
        postData[this.name] = this.value;
     });
-    console.log(data);
+    // console.log(data);
     var url = SCOPE.listorder_url;
     $.post(url,postData,function(result){
         if(result.status == 1) {
@@ -136,33 +136,3 @@ $('.singcms-table #singcms-on-off').on('click', function(){
 
 });
 
-/**
- * 推送JS相关
- */
-$("#singcms-push").click(function(){
-    var id = $("#select-push").val();
-    if(id==0) {
-        return dialog.error("请选择推荐位");
-    }
-    push = {};
-    postData = {};
-    $("input[name='pushcheck']:checked").each(function(i){
-        push[i] = $(this).val();
-    });
-
-    postData['push'] = push;
-    postData['position_id']  =  id;
-    //console.log(postData);return;
-    var url = SCOPE.push_url;
-    $.post(url, postData, function(result){
-        if(result.status == 1) {
-            // TODO
-            return dialog.success(result.message,result['data']['jump_url']);
-        }
-        if(result.status == 0) {
-            // TODO
-            return dialog.error(result.message);
-        }
-    },"json");
-
-});
